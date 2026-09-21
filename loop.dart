9156ac59a112b9d900f67ebd82228bcc98e366cc
Adeel Hussain Gilal <adeelhussaingilal@gmail.com>
2026-09-21 15:52:47 +0500

    uptade

---
+++ loop.dart
@@ -0,0 +1,28 @@
+void main() {
+  // FOR LOOP
+  print("For Loop:");
+
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+  }
+
+  // WHILE LOOP
+  print("\nWhile Loop:");
+
+  int j = 1;
+
+  while (j <= 5) {
+    print(j);
+    j++;
+  }
+
+  // DO-WHILE LOOP
+  print("\nDo-While Loop:");
+
+  int k = 1;
+
+  do {
+    print(k);
+    k++;
+  } while (k <= 5);
+}
